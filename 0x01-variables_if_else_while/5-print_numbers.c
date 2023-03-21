2cdbebe1bd7fa89be79243356ae0f86501d73ede
humphreygitonga <humgitonga234@gmail.com>
2023-03-21 09:26:25 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description:'prints all single digit numbers of base 10'
+ * Description: 'prints all single digit numbers of base 10'
  * return: always 0
  */
 int main(void)
