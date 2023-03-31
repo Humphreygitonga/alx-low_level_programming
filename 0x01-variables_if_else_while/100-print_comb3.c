8b7a1fca9466c91c9d9efbaf0fc632e9455c3f1c
humphreygitonga <humgitonga234@gmail.com>
2023-03-31 02:47:22 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int n, m;
 
-	for(n = 48; n <= 56; n++)
+	for (n = 48; n <= 56; n++)
 	{
-		for(m = 49; m <= 57; m++)
+		for (m = 49; m <= 57; m++)
 		{
-			if(m > n)
+			if (m > n)
 			{
 				putchar(n);
 				putchar(m);
-				if(n != 56 || m != 57)
+				if (n != 56 || m != 57)
 				{
 					putchar(',');
 					putchar(' ');
@@ -25,5 +25,5 @@ int main(void)
 		}
 	}
 	putchar('\n');
-	return(0);
+	return (0);
 }
