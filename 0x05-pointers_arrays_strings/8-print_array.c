7ea1a9fc68fc2f95e099e5670b30c282c66338ce
humphreygitonga <humgitonga234@gmail.com>
2023-04-02 03:50:10 -0700

    100-atoi.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,4 +1,5 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
 
 /**
  * print_array - prints n elements of any array of integers
@@ -6,16 +7,16 @@
  * @a: array to be printed
  * @n: number of elements to print
  */
-void print_arrat(int *a, int n)
+void print_array(int *a, int n)
 {
 	int i;
 
 	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-			printf("%d", a[i]);
+			printf ("%d", a[i]);
 		else
-			printf(", %d", a[i]);
+			printf (", %d", a[i]);
 	}
-	printf("\n");
+	printf ("\n");
 }
