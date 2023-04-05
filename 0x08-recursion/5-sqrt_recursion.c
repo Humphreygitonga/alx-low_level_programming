533a8fad4719f29822817d154fd4d78d5b640e62
humphreygitonga <humgitonga234@gmail.com>
2023-04-05 07:01:58 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,7 +16,7 @@ int _sqrt_recursion(int n)
 }
 
 /**
- * actual_sqrt_recursion - recurses to find the natural square root 
+ * actual_sqrt_recursion - recurses to find the natural square root
  * of a number
  * @n: number to calculate the square root of
  * @i: iterator
@@ -29,5 +29,5 @@ int actual_sqrt_recursion(int n, int i)
 		return (-1);
 	if (i * i == n)
 		return (i);
-	return (actual_sqrt_recursion(n, i +1));
+	return (actual_sqrt_recursion(n, i + 1));
 }
