453c43c3c242a5f0a617136cb6813e96779798e3
humphreygitonga <humgitonga234@gmail.com>
2023-04-26 09:14:41 -0700

    0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,15 +20,15 @@ char *create_array(unsigned int size, char c)
 	p = (char *) malloc(sizeof(char) * size);
 
 	if (p == NULL)
-		return(0);
+		return (0);
 
 	while (i < size)
 	{
-		*(p+i) = c;
+		*(p + i) = c;
 		i++;
 	}
 
 	*(p+i) = '\0';
-	
-	return(p);
+
+	return (p);
 }
